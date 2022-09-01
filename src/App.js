00cea5ebb08d6ce182402c6bb4a519b9276c5ede
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-09-01 15:16:00 +0200

    Add npm's and styling and link to Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Trainer from "./Page/Trainer";
+import Welcome from "./Templates/Welcome";
+import Home from "./Templates/Home";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Page/Trainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Welcome from '../Templates/Welcome'
+
+const Trainer = () => {
+    return (
+        <div>
+            <div>
+                <Welcome />
+            </div>
+        </div>
+    )
+}
+
+export default Trainer
