f6e54250f96c7bb6e3b22fbc34d3cf4ea5111204
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-09-05 09:48:36 +0200

    add burger menu and styling

---
+++ src/Components/HeaderBurgerMenu.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { CgClose } from "react-icons/cg";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderBurgerMenu = () => {
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    return (
+        <div>
+            <nav>
+                <section className=' text-[24px]'>
+                    <div onClick={() => setIsNavOpen((prev) => !prev)} className=' text-[#9e9e9e] text-xl'>
+                        <HiMenuAlt3 />
+                    </div>
+                    <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                        <div
+                            className="absolute top-0 right-0 px-8 py-8 text-[#9e9e9e] text-xl"
+                            onClick={() => setIsNavOpen(false)}
+                        ><CgClose />
+                        </div>
+                        <ul className="flex flex-col items-center justify-between min-h-[250px] ">
+                            <Link to="/Home">
+                                <li className=" my-8 ">
+                                    <button>Home</button>
+                                </li>
+                            </Link>
+                            <Link to="/About">
+                                <li className=" my-8 ">
+                                    <button>Search</button>
+                                </li>
+                            </Link>
+                            <Link to="/About">
+                                <li className=" my-8 ">
+                                    <button>My Schedule</button>
+                                </li>
+                            </Link>
+                            <Link to="/About">
+                                <li className=" my-8 ">
+                                    <button>Log in</button>
+                                </li>
+                            </Link>
+
+                        </ul>
+                    </div>
+                </section>
+            </nav>
+        </div>
+    )
+}
+
+export default HeaderBurgerMenu
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRef } from 'react';
+import HeaderBurgerMenu from '../Components/HeaderBurgerMenu';
 import HomeFirstClass from '../Components/HomeFirstClass';
 import HomeMultipleClasses from '../Components/HomeMultipleClasses';
 
@@ -7,25 +8,27 @@ const Home = () => {
     const constraintsRef = useRef(null)
 
     return (
-        <div className='flex flex-col gap-16'>
-            <div>
-                <div className='pl-4 pt-9'>
-                    <h2 className='font-medium'>Popular classes</h2>
-                </div>
+        <div>
+            <div className='flex flex-col gap-16'>
                 <div>
-                    <HomeFirstClass />
+                    <div className='p-6 flex justify-between'>
+                        <h2 className='font-medium'>Popular classes</h2>
+                        <HeaderBurgerMenu />
+                    </div>
+                    <div>
+                        <HomeFirstClass />
+                    </div>
+                </div>
+                <div className='place-content-end'>
+                    <div className='pl-4'>
+                        <h2 className='font-bold'>Classes for you</h2>
+                    </div>
+                    <div ref={constraintsRef} className=''>
+                        <HomeMultipleClasses />
+                    </div>
                 </div>
             </div>
-            <div className='place-content-end'>
-                <div className='pl-4'>
-                    <h2 className='font-bold'>Classes for you</h2>
-                </div>
-                <div ref={constraintsRef} className=''>
-                    <HomeMultipleClasses />
-
-                </div>
-            </div>
-        </div >
+        </div>
     )
 }
 
