13b4d94a44a87b4618e0befe6d35db444d649bc2
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-09-05 09:22:17 +0200

    add styling to home

--- src/Components/HomeFirstClass.js
+++ src/Components/HomeFirstClass.js
@@ -3,33 +3,50 @@ import { useState, useEffect } from 'react';
 import { AiFillStar } from "react-icons/ai";
 
 const HomeFirstClass = () => {
-
-    const [firstClass, setFirstClass] = useState([]);
-
+    const [randomClass, setRandomClass] = useState([]);
 
     useEffect(() => {
         fetch(
-            'http://localhost:4000/api/v1/classes/3',
+            'http://localhost:4000/api/v1/classes',
             {
                 method: 'GET',
             }
         )
             .then(res => res.json())
-            .then(data => setFirstClass(data))
+            .then(data => setRandomClass(data))
             .catch(err => console.log(err))
-    }, [])
+    }
+        , [])
+
+    const randomImage = () => {
+        const random = Math.floor(Math.random() * randomClass.length);
+        return randomClass[random].asset.url;
+    }
+
+    const randomName = () => {
+        const random = Math.floor(Math.random() * randomClass.length);
+        return randomClass[random].className;
+    }
+
+
 
     return (
         <div>
-            <div className='flex'>
-                <div className='p-4'>
-                    <div className='absolute'>
-                        <img src={firstClass.asset && firstClass.asset.url} alt="classes" className='rounded-xl' />
+            <div className='flex px-5'>
+                {
+                    randomClass.length > 0 && <div className=' h-[500px] w-[350px] rounded-xl'
+                        style={{
+                            backgroundImage: `url(${randomImage()})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                    >
+                        <button className='bg-[#f1c40e] p-4 mt-[427px] rounded-bl-lg rounded-tr-lg'>
+                            {randomName()} <p className='flex justify-evenly p-1'><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /></p>
+                        </button>
                     </div>
-                    <div className='relative'>
-                        <button className='bg-[#f1c40e] font-medium rounded-tr-lg rounded-bl-lg text-[12px] mt-[122px] p-4 relative'>{firstClass.className} <p className='flex pt-2 justify-evenly'><AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar /></p> </button>
-                    </div>
-                </div>
+                }
             </div>
         </div>
     )
